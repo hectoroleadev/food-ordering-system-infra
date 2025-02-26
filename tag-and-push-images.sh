0ff83ba35b015a0ccbb0a03c186f9234c18460a5
Hector Olea <imacako@gmail.com>
2025-02-25 22:51:38 -0600

    infras changes

---
+++ tag-and-push-images.sh
@@ -0,0 +1,15 @@
+gcloud auth login
+
+docker tag dev.hectorolea.food.ordering.system/customer.service:1.0-SNAPSHOT us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/customer.service:$1
+
+docker tag dev.hectorolea.food.ordering.system/payment.service:1.0-SNAPSHOT us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/payment.service:$1
+
+docker tag dev.hectorolea.food.ordering.system/order.service:1.0-SNAPSHOT us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/order.service:$1
+
+docker tag dev.hectorolea.food.ordering.system/restaurant.service:1.0-SNAPSHOT us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/restaurant.service:$1
+
+docker push us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/customer.service:$1
+docker push us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/payment.service:$1
+docker push us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/restaurant.service:$1
+docker push us-central1-docker.pkg.dev/food-ordering-system-452021/food-ordering-system-repository/order.service:$1
+
